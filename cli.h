0c7671722654f8d6113c8881ea3bf0a471355456
Zack Schrage <zjs5@duke.edu>
2023-05-15 22:38:51 +0800

    added documentation to cli.h

--- cli.h
+++ cli.h
@@ -13,7 +13,7 @@ touch [name] [size]   DONE
 mkdir [name]          DONE
 pwd                   DONE
 ls                    DONE
-cd [name]             BUGGY
+cd [name]             BUGGY (cannot cd into bad directory)
 rm [name]             LIMITED
 rmdir [name]          LIMITED
 exit                  DONE
@@ -22,12 +22,80 @@ read [file]
 chmod [file] [flags]
 */
 
+/**
+ * @brief Starts CLI simulation interface in console
+ * Usage: Enter the above keywords followed by optional parameters
+ * Special escape sequence "quit" used to terminate program
+ * 
+ * @param fs filesystem object
+ */
 void run_cli_sim(FileSystem* fs);
+
+/**
+ * @brief Redirects user input into the appropriate function given the keyword
+ * 
+ * @param fs filesystem object
+ * @param line user command line entry 
+ * @param size length of command line entry
+ * @param type numerical index of keyword
+ */
 void redirect_func_call(FileSystem* fs, char* line, int size, int type);
+
+/**
+ * @brief Creates a file with name and size in current working directory
+ * 
+ * @param fs filesystem object
+ * @param name file name
+ * @param size file size
+ */
 void touch(FileSystem* fs, char* name, int size);
+
+/**
+ * @brief Creates a directory with name in current working directory
+ * 
+ * @param fs filesystem object
+ * @param name directory name
+ */
 void mkdir(FileSystem* fs, char* name);
+
+/**
+ * @brief Prints the current working directory
+ * 
+ * @param fs filesystem object
+ */
 void pwd(FileSystem* fs);
+
+/**
+ * @brief Lists the children files and directories of current working directory
+ * 
+ * @param fs filesystem object
+ */
 void ls(FileSystem* fs);
+
+/**
+ * @brief Changes into new directory given three use cases
+ * 1) If the provided path is an absolute path
+ * 2) If the provided path is relative path (child name only, omits forward slash)
+ * 3) Two dots indicating to move to parent directory
+ * 
+ * @param fs filesystem object
+ * @param name directory path
+ */
 void cd(FileSystem* fs, char* name);
+
+/**
+ * @brief Remove a file
+ * 
+ * @param fs filesystem object
+ * @param name file name
+ */
 void rm(FileSystem* fs, char* name);
+
+/**
+ * @brief Remove a directory. 
+ * Will remove recursively and eliminate all children files and directoroes
+ * 
+ * @param fs filesystem object
+ * @param name directory name
+ */
 void rmdir(FileSystem* fs, char* name);
