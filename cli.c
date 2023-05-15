e957da9ca4b9815ec5c3d26a940ed7ecd0cf7a61
Zack Schrage <zjs5@duke.edu>
2023-05-15 15:09:03 +0800

    keywords added to cli sim

--- cli.c
+++ cli.c
@@ -1,16 +1,22 @@
 #include "cli.h"
 
 const int NUM_KEY_WORDS = 7;
-const char* KEY_WORDS[NUM_KEY_WORDS] = {"touch", "mkdir", "pwd", "ls", "cd", "rm", "rmdir"};
+char* KEY_WORDS[NUM_KEY_WORDS] = {"touch", "mkdir", "pwd", "ls", "cd", "rm", "rmdir"};
 
 void run_cli_sim() {
-    printf("%s\n", KEY_WORDS[0]);
-    printf("%s\n", KEY_WORDS[1]);
     while (1) {
         char* line = NULL;
         size_t len = 0;
         ssize_t lineSize = 0;
         lineSize = getline(&line, &len, stdin);
-        printf("You entered %s, which has %zu chars.\n", line, lineSize -1);
+        if (prefix_matching("quit\n", line, ' ')) return;
+        int valid = 0;
+        for (int i = 0; i < NUM_KEY_WORDS; i++) {
+            if (prefix_matching(KEY_WORDS[i], line, ' ') || prefix_matching(KEY_WORDS[i], line, '\n')) {
+                valid = 1;
+                printf("matching %s\n", KEY_WORDS[i]);
+            }
+        }
+        if (!valid) printf("Not a valid command\n");
     }
 }
